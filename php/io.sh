965cee43964ac70b619530fb1211e95678218387
atelierbram <bramdeh@gmail.com>
2014-07-29 14:38:34 +0200

    fixed absolute paths to files in shell script

--- php/io.sh
+++ php/io.sh
@@ -4,9 +4,9 @@
 write_to_file()
 {
 
- curl "http://localhost/~bram/php/picture-srcset/php/index.php" > "../index.html"
+curl "http://localhost/~bram/php/picture-srcset/repo/picture-element-test/php/index.php" > "../index.html"
 
-curl "http://localhost/~bram/php/picture-srcset/php/index-external.php" > "../index-external.html"
+curl "http://localhost/~bram/php/picture-srcset/repo/picture-element-test/php/index-external.php" > "../index-external.html"
 }
 
 # execute it
